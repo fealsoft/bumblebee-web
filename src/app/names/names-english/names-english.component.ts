3761d2d3bed14bd6ef57b0f09294bf2db896fd42
Fedotov <oleksandr.fedotov@vitech.com.ua>
2022-08-26 09:51:37 +0300

    Added Names.

--- src/app/names/names-english/names-english.component.ts
+++ src/app/names/names-english/names-english.component.ts
@@ -12,7 +12,34 @@ export class NamesEnglishComponent {
   constructor(private dataService: DataService) {
     this.dataService.allEnglishNames().subscribe((data: NameEng[]) => {
       this.nameEngs = data;
+
+      if(this.nameEngs !== undefined) {
+        this.selectedValue = this.nameEngs[0].id;
+        this.setSelectedObject();
+        this.countValues = this.nameEngs.length;
+      }
     });
+  }
+
+  selectedValue: any | undefined;
+  selectedObject: any | undefined;
+
+  countValues: number = 0;
+
+  setSelectedObject() {
+    if(this.nameEngs !== undefined) {
+      this.selectedObject = this.nameEngs.find((el: any) => {
+        return el?.id == this.selectedValue;
+      })
+    }
+  }
+
+  getName() {
+    return this.selectedObject !== undefined ? this.selectedObject.name : ""
+  }
+
+  getGenderName() {
+    return this.selectedObject !== undefined ? this.selectedObject.genderName : ""
   }
 
 }
--- src/app/names/names-ukrainian/names-ukrainian.component.ts
+++ src/app/names/names-ukrainian/names-ukrainian.component.ts
@@ -12,7 +12,35 @@ export class NamesUkrainianComponent {
   constructor(private dataService: DataService) {
     this.dataService.allUkrainianNames().subscribe((data: NameUkr[]) => {
       this.nameUkrs = data;
+
+      if(this.nameUkrs !== undefined) {
+        this.selectedValue = this.nameUkrs[0].id;
+        this.setSelectedObject();
+        this.countValues = this.nameUkrs.length;
+      }
     });
   }
+
+  selectedValue: any | undefined;
+  selectedObject: any | undefined;
+
+  countValues: number = 0;
+
+  setSelectedObject() {
+    if(this.nameUkrs !== undefined) {
+      this.selectedObject = this.nameUkrs.find((el: any) => {
+        return el?.id == this.selectedValue;
+      })
+    }
+  }
+
+  getName() {
+    return this.selectedObject !== undefined ? this.selectedObject.name : ""
+  }
+
+  getGenderName() {
+    return this.selectedObject !== undefined ? this.selectedObject.genderName : ""
+  }
+
 
 }
